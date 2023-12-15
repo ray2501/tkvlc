7718ab542b73d918db125ebceecdeed5a434a051
Danilo Chang <ray2501@gmail.com>
2023-12-15 17:37:04 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tkvlc 0.8 \
-    [list load [file join $dir libtkvlc0.8.so] tkvlc]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tkvlc 0.8 \
+	    [list load [file join $dir libtcl9tkvlc0.8.so] [string totitle tkvlc]]
+} else {
+    package ifneeded tkvlc 0.8 \
+	    [list load [file join $dir libtkvlc0.8.so] [string totitle tkvlc]]
+}
